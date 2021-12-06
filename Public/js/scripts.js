3c16305821c09d9d58d7c3ae1430f28b0821f549
kleitomberg <ozkberguinho@gmail.com>
2021-12-06 00:11:48 -0300

    projeto quase finalizado

---
+++ Public/js/scripts.js
@@ -0,0 +1,29 @@
+//pegamos a navbar ou seja a "Ul"
+var navbar = document.getElementById("nav");
+
+
+//dentro da Ul, pegamos todos os elementos com a class, navlink ou seja os "a"
+var links = navbar.getElementsByClassName("nav-link");
+
+//Agora de acordo com a quantidade de links repita um evento de clique na class 
+//que foi clicada acionando um evento que abre uma função que executa o processo de pegar as classes active ou seja so tem uma
+//então como é um array, pegamos a posição 0 e trocamos por "" 
+//ou seja pegamos a classe que tinha activre antes retiramos dela e adicionamos na navlink que foi clicada
+
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+/** Scroll Reveal */
+
+ScrollReveal().reveal('.home', { delay: 50 });
+ScrollReveal().reveal('.sobre', { delay: 150 });
+ScrollReveal().reveal('.sobre', { delay: 350 });
+ScrollReveal().reveal('.solucao', { delay: 450 });
+ScrollReveal().reveal('.contato', { delay: 550 });
+ScrollReveal({ reset: true });
