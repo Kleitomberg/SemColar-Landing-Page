df5484f1590a83bf68853f80f87668fccb1551ed
kleitomberg <ozkberguinho@gmail.com>
2021-12-06 01:31:18 -0300

    botão fixo

--- Public/js/scripts.js
+++ Public/js/scripts.js
@@ -27,3 +27,25 @@ ScrollReveal().reveal('.sobre', { delay: 350 });
 ScrollReveal().reveal('.solucao', { delay: 450 });
 ScrollReveal().reveal('.contato', { delay: 550 });
 ScrollReveal({ reset: true });
+
+
+
+ //Get the button
+ var mybutton = document.getElementById("myBtn");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+   if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 1500) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
